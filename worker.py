db468c7effb87e242dbf1ee2fbc4a3186c24192c
andrey.sysenko <andrey.sysenko@murka.com>
2025-12-18 19:05:38 +0200

    fix lora

--- worker.py
+++ worker.py
@@ -389,10 +389,12 @@ def build_workflow_from_payload(workflow_key: str, payload: dict) -> dict:
         # числа
         elif isinstance(value, (int, float)):
             replacement = str(value)
+        elif key == 'lora_nodes'
+            replacement = str(value)
         else:
             s = str(value)
-            #dumped = json.dumps(s, ensure_ascii=False)
-            replacement = s #dumped[1:-1]  # викидаємо зовнішні лапки
+            dumped = json.dumps(s, ensure_ascii=False)
+            replacement = dumped[1:-1]  # викидаємо зовнішні лапки
 
         # І тупо заміняємо в тексті ВСІ входження param_<key>
         txt = txt.replace(placeholder, replacement)
